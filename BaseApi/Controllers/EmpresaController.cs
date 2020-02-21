17f6cff5ebf938439fed52dd746a23bc3bcd1241
Victor Hugo <victorhugo-5@hotmail.com>
2020-02-21 17:41:15 -0300

    Criação da controller empresa

---
+++ BaseApi/Controllers/EmpresaController.cs
@@ -0,0 +1,25 @@
+﻿using BaseApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseApi.Controllers
+{
+    [ApiController]
+    [Route("Api/Empresa")]
+    public class EmpresaController : ControllerBase
+    {
+        private readonly DbTesteContext contexto;
+
+        public EmpresaController(DbTesteContext context) {
+            contexto = context;
+        }
+
+        [HttpGet("GetDocuments/{numdoc?}")]
+        public void GetDocuments() {
+
+        }
+    }
+}
